ca1ff6a70ef3e9c0c509b418c6d77afc632a8c29
Willian Mano <willianmanoaraujo@gmail.com>
2025-11-05 13:32:23 -0300

    Adiciona traducao para pt-br

---
+++ lang/pt_br/proposal.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_proposal
+ * @category    string
+ * @copyright   2025 Willian Mano <willianmano@conecti.me>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['missingidandcmid'] = 'Faltando ID e cmid';
+$string['modulename'] = 'Propostas';
+$string['modulename_help'] = 'Use o módulo propostas para criar uma atividade onde pessoas fazem sugestão de propostas e votam umas nas outras.';
+$string['modulenameplural'] = 'Propostas';
+$string['noproposalinstances'] = 'Sem propostas';
+$string['pluginadministration'] = 'Administrar propostas';
+$string['pluginname'] = 'Módulo propostas';
+$string['privacy:metadata'] = 'O módulo propostas não guarda dados pessoais';
+$string['proposal:addinstance'] = 'Adicionar instância';
+$string['proposal:view'] = 'Visualizar';
+$string['proposalsettings'] = 'Configurações';
+$string['view'] = 'Visualizar';
+$string['completionsubmit'] = 'Enviar uma proposta';
+$string['completionproposal'] = 'Adicionar uma proposta';
+$string['content'] = 'Conteúdo';
+$string['difficulty'] = 'Dificuldade';
+$string['easy'] = 'Fácil';
+$string['medium'] = 'Médio';
+$string['hard'] = 'Difícil';
+$string['entry:add'] = 'Adicionar uma nova proposta';
+$string['entry:add_success'] = 'Proposta adicionada com sucesso';
+$string['entry:update_success'] = 'Proposta atualizada com sucesso';
+$string['cannotrateyourownentry'] = 'Você não pode votar na sua própria proposta';
